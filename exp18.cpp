67bb77b4cef5f65b5e2a6a571a44a0c1f81485d3
ansh003 <60300338+ansh003@users.noreply.github.com>
2023-08-03 10:40:34 +0530

    Add files via upload

---
+++ exp18.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,m,n ;
+    int matrix[3][2]={{1,2},{4,5},{7,8}};
+    for(i=0;i<3;i++)
+    {
+        cout<<endl;
+        for(j=0;j<2;j++)
+        {
+            cout << matrix[i][j]<<"\t";
+        }
+    }
+    int matrix1[3][2]={{2,1},{5,4},{8,7}};
+    for(m=0;i<3;m++)
+    {
+        cout<<endl;
+        for(n=0;n<2;n++)
+        {
+            cout << matrix1[m][n]<<"\t";
+        }
+    }
+    int matrix[3][2]=
+    return 0;
+}
